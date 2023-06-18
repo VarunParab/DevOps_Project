212eed62f5d80cd6f33c2a8d736fc0a83e3ed257
VarunParab <varunparab7@gmail.com>
2023-06-19 00:31:34 +0530

    first

---
+++ Registration/registerPage.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
+// Create a DynamoDB client instance
+const dynamodbClient = new DynamoDBClient({
+    region: 'ap-south-1',
+    credentials: {
+      accessKeyId: 'AKIAX3GW77ZAVZ3S64W2',
+      secretAccessKey: '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'
+    }
+  }); 
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.post('/register', async(req, res) => {
+  const { email } = req.body;
+
+  const putItemCommand = new PutItemCommand({
+    TableName: 'Table',
+    Item: {
+      'userid': { S: email }
+    }
+  });
+  try {
+    await dynamodbClient.send(putItemCommand);
+    console.log('Email ID saved successfully');
+    res.status(200).json({ message: 'Email ID saved successfully' });
+  } catch (error) {
+    console.error('Error saving email ID to DynamoDB:', error);
+    res.status(500).json({ error: 'Failed to save email ID' });
+  }
+});
+
+app.listen(4000, () => {
+    console.log("Listening on 4000");
+});
